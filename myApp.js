4efb8e549ab71cb6524daeada49a3313ca52f221
Shrishti <shrishti2454soam@gmail.com>
2021-05-10 17:02:05 +0530

    env variable

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('dotenv').config(); //including env_var 
 
 app.get('/',function(req,res){
     res.sendFile(__dirname + "/views/index.html");
@@ -8,9 +9,16 @@ app.get('/',function(req,res){
 app.use("/public",express.static(__dirname + "/public"));
 
 app.get('/json',function(req,res){
+    console.log(process.env.MESSAGE_STYLE+ "something?");
+    if(process.env.MESSAGE_STYLE == "uppercase"){
+        res.json(
+            {"message": "HELLO JSON"}
+            );
+    }else{
     res.json(
         {"message": "Hello json"}
         );
+    }
 })
 
 console.log("Hello World");
